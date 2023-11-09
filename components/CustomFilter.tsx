3a82de91bea1addc7b985e6097dafe367c91e829
BorisP99 <bpajovic99@gmail.com>
2023-11-09 12:17:33 +0100

    Update CustomFilter.tsx

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -2,7 +2,6 @@
 
 import { Fragment, useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 
 import { Listbox, Transition } from "@headlessui/react";
 
